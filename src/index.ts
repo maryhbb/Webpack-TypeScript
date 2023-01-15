0663df3d298afe29a2bef5603b91c341b653dd9e
Mary <mary_hbb@yahoo.com>
2023-01-15 17:29:48 +0100

    Update ts file

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,19 @@
-class user {
-    firstname: String = "Mary";
-    lastname:String = "Habibi";
-    age: number = 20;
+class User {
+    firstname: String ;
+    lastname:String ;
+    age: number;
 
+    public constructor (firstname: string, lastname: string, age: number){
+        this.firstname = firstname;
+        this.lastname = lastname;
+        this.age = age;
+    }
 }
 
-const myUser = new user;
-console.log(myUser.firstname, myUser.lastname, myUser.age);
+const myUser = new User("Mary", "HBB", 20);
+const myUser2 = new User("Mani", "HBB", 17);
+const myUser3= new User("JOJO", "JD", 40);
+
+console.log(myUser);
+console.log(myUser2);
+console.log(myUser3);
