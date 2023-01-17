ff30013225c65fdc2a317874205a67f1c3c320eb
Mary <mary_hbb@yahoo.com>
2023-01-17 04:08:08 +0100

    Install css plugn and Configure webpack.config

--- webpack.config.js
+++ webpack.config.js
@@ -1,27 +1,36 @@
-const path = require('path');
-const HtmlWebpackPlugin = require('html-webpack-plugin');
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 
 module.exports = {
-  entry: './src/index.ts',
+  entry: "./src/index.ts",
   module: {
     rules: [
       {
         test: /\.ts$/,
-        use: 'ts-loader',
+        use: "ts-loader",
         exclude: /node_modules/,
+      },
+      {
+        test: /\.css$/i,
+        use: [MiniCssExtractPlugin.loader, "css-loader"],
+        // use: ["style-loader", "css-loader"],
       },
     ],
   },
   resolve: {
-    extensions: [ '.ts' , '.js'],
+    extensions: [".ts", ".js"],
   },
   output: {
-    filename: 'bundle.js',
-    path: path.resolve(__dirname, 'public'),
-    clean: true
+    filename: "bundle.js",
+    path: path.resolve(__dirname, "public"),
+    clean: true,
   },
-  plugins: [new HtmlWebpackPlugin({
-    title: "Mary's App",
-    template: 'src/index.html'
-  })]
+  plugins: [
+    new MiniCssExtractPlugin(),
+    new HtmlWebpackPlugin({
+      title: "Mary's App",
+      template: "src/index.html",
+    }),
+  ],
 };
